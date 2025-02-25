af75a86031e7d6cff5f2d25f0be4f248082b6a2c
Dmitry <drashepkin9@gmail.com>
2025-02-25 09:42:15 +0300

    Add Gauss repaired

---
+++ Gauss/Gauss.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauss
+{
+	internal class Gauss
+	{
+		private static float[,] result;
+
+		public static float[,] Count()
+		{
+			result = new float[Program.n, Program.m];
+			Array.Copy(Program.matrix, result, result.Length);
+			// result = new float[Program.n, Program.m];
+
+			for (int i = 0; i < Program.indexes.Length - 1; i++)
+			{
+				for (int j = i + 1; j < Program.indexes.Length; j++)
+				{
+					float koef = (float)result[j, Program.indexes[i]] / (float)result[i, Program.indexes[i]];
+					SubtractLine(j, i, koef);
+				}
+			}
+
+            for (int i = Program.indexes.Length - 1; i > 0; i--)
+			{
+				for (int j = i - 1; j >= 0 ; j--)
+				{
+					float koef = (float)result[j, Program.indexes[i]] / (float)result[i, Program.indexes[i]];
+					SubtractLine(j, i, koef);
+				}
+			}
+
+            return result;
+		}
+
+		private static void SubtractLine(int k, int l, float koef)
+		{
+			for (int i = 0; i < Program.m; i++)
+			{
+				result[k, i] -= result[l, i] * koef;
+			}
+		}
+	}
+}
---
+++ Gauss/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Numerics;
+using System.Linq.Expressions;
+
+namespace Gauss
+{
+	internal class Program
+	{
+        private const string fileIn = "input.txt";
+        private const string fileOut = "output.txt";
+        private static StreamReader reader = new StreamReader(fileIn);
+        private static StreamWriter writer = new StreamWriter(fileOut);
+        public static int n, m;
+        public static int[] indexes;
+        public static float[,] matrix;
+
+        static void Main(string[] args)
+		{
+			GetMatrix();
+            float[,] result = Gauss.Count();
+
+            for (int i = 0; i < n; i++)
+			{
+                for (int j = 0; j < m; j++)
+                {
+					writer.Write(result[i, j]);
+                    writer.Write("\t");
+                }
+                writer.Write("\n");
+            }
+
+			reader.Close();
+            writer.Close();
+			return;
+        }
+
+		private static void GetMatrix()
+		{
+			int[] buffer;
+
+			try
+			{
+				buffer = GetIntegerLine();
+				if (buffer.Length != 2)
+				{
+					throw new FormatException("Wrong format");
+				}
+			}
+			catch (FormatException err)
+			{
+				Console.WriteLine(err.Message);
+				return;
+			}
+			n = buffer[0];
+			m = buffer[1];
+
+			try
+			{	
+				indexes = GetIntegerLine();
+				if (indexes.Length == 0 || indexes.Length > m)
+				{
+					throw new FormatException("Wrong format: wrong indexes");
+				}
+			}
+			catch (FormatException err)
+			{
+				Console.WriteLine(err.Message);
+				return;
+			}
+
+			matrix = new float[n, m];
+			for (int i = 0; i < n; i++)
+			{
+				try
+				{
+					buffer = GetIntegerLine();
+					if (buffer.Length != m)
+					{
+						throw new FormatException("Wrong format");
+					}
+				}
+				catch (FormatException err)
+				{
+					Console.WriteLine(err.Message);
+					return;
+				}
+				for (int j = 0; j < m; j++)
+				{
+					matrix[i, j] = buffer[j];
+				}
+			}   
+		}
+
+		private static int[] GetIntegerLine()
+		{
+			int i, buf;
+			int[] result;
+			string? str = reader.ReadLine();
+			if (str == null)
+			{
+				throw new FormatException("Wrong format: str null");
+			}
+			string[] strs = str.Split(' ');
+
+			result = new int[strs.Length];
+			for (i = 0; i < strs.Length; i++)
+			{
+				try
+				{
+					buf = Convert.ToInt32(strs[i]);
+				}
+				catch (FormatException)
+				{
+					throw;
+				}
+				result[i] = buf;
+			}
+
+			return result;
+		}
+	}
+}
